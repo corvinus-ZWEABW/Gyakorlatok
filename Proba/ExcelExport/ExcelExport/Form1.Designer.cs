9c2b3fc2ce7fb0f4296bbe3d348c5b1ecdd5c132
corvinus-ZWEABW <encsi1117@gmail.com>
2021-10-08 10:47:37 +0200

    Create Excel Workbook

--- Proba/ExcelExport/ExcelExport/Form1.Designer.cs
+++ Proba/ExcelExport/ExcelExport/Form1.Designer.cs
@@ -36,10 +36,11 @@ namespace ExcelExport
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(64, 68);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(604, 426);
             this.dataGridView1.TabIndex = 0;
             // 
             // Form1
